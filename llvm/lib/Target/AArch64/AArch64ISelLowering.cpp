784b663e6619fedf4c85cfc1a7780f84cc3e7a3a
Jonathan Thackray <jonathan.thackray@arm.com>
2026-01-08 16:08:36 +0000

    [AArch64][llvm] Allow FPRCVT insns to run in streaming mode if safe
    For FEAT_FPRCVT instructions, allow them to run in streaming mode if safe

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -20681,6 +20681,9 @@ tryToReplaceScalarFPConversionWithSVE(SDNode *N, SelectionDAG &DAG,
   if (DCI.isBeforeLegalizeOps())
     return SDValue();
 
+  if (Subtarget->isStreaming() && Subtarget->hasFPRCVT())
+    return SDValue();
+
   if (!Subtarget->isSVEorStreamingSVEAvailable() ||
       (!Subtarget->isStreaming() && !Subtarget->isStreamingCompatible()))
     return SDValue();
